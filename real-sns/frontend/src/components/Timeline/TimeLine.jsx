4d70f4e0e143f9ab1472c8325443c125ad4095e7
shunma <shunmakajioka12@gmail.com>
2024-02-29 17:05:06 +0900

    axiosを使ってapi叩く

--- real-sns/frontend/src/components/Timeline/TimeLine.jsx
+++ real-sns/frontend/src/components/Timeline/TimeLine.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './TimeLine.css'
 import Share from '../share/Share'
 import Post from '../post/Post'
-import { Posts } from '../../dummyData'
+// import { Posts } from '../../dummyData'
+import axios from 'axios';
 
 const TimeLine = () => {
+
+  const [posts, setPosts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+    const responce =  await axios.get("/posts/timeline/65d76e837c22e4960fc01cc7")
+    // console.log(responce)
+    setPosts(responce.data)
+    }
+    fetchPosts();
+  },[])
+
+
   return (
     <div className='TimeLine'>
       <div className='TimeLinewrapper'>
         <Share/>
-       {Posts.map((post) => (
-        <Post post={post} key={post.id}/>
-       ))}
+        {posts.map((post) => (
+  <Post key={post._id} post={post} />
+))}
       </div>
     </div>
   )
--- real-sns/frontend/src/components/post/Post.jsx
+++ real-sns/frontend/src/components/post/Post.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
 import './Post.css'
 import { MoreVert } from '@mui/icons-material'
-import { Users } from '../../dummyData'
+// import { Users } from '../../dummyData'
+import {format} from 'timeago.js';
+
 
 const Post = ({post}) => {
 
@@ -10,8 +13,22 @@ const Post = ({post}) => {
     // const user = Users.filter(user => user.id ===  1);
     // console.log(user[0].username)
 
-    const [like, setLike] = useState(post.like);
+    const [like, setLike] = useState(post.likes.length);
     const [isliked, setIsliked] = useState(false)
+
+    const [user, setUser] = useState({});
+
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+    const responce =  await axios.get(`/users/${post.userId}`)
+   console.log(responce)
+    setUser(responce.data)
+    }
+    fetchUsers();
+  },[post.userId])
+
+
 
     const handleLike = () => {
         setLike(isliked ? like -1 : like + 1);
@@ -27,9 +44,9 @@ const Post = ({post}) => {
         <div className='postWrapper'>
             <div className='postTop'>
               <div className='postTopleft'>
-                <img src={PUBLIC_FOLDER + Users.filter(user => user.id ===  post.id)[0].profilePicture} alt="" className="postProfileImg"/>
-                <span className='postUsername'>{ Users.filter(user => user.id ===  post.id)[0].username}</span>
-                <span className='postDate'>{post.date}</span>
+                <img src={user.profilePicture || PUBLIC_FOLDER  +  "/person/noAvatar1.png"} alt="" className="postProfileImg"/>
+                <span className='postUsername'>{ user.username}</span>
+                <span className='postDate'>{format(post.createdAt)}</span>
             </div>
             <div className='postTopRight'>
                 <MoreVert/>
@@ -37,7 +54,7 @@ const Post = ({post}) => {
             </div>
             <div className='postCenter'>
                 <span className='postText'>{post.desc}</span>
-                <img src={PUBLIC_FOLDER + post.photo} alt="" className='postImg'/>
+                <img src={PUBLIC_FOLDER + post.img} alt="" className='postImg'/>
             </div>
             <div className='postButtom'>
                 <div className='postButtonleft'>
